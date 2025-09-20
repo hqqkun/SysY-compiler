db6e99b8b1953455278f8454b4a30081901c98a1
hqqkun <hqqkun1997@163.com>
2025-09-20 13:10:46 +0000

    feat: support better reg alloc

--- src/IR/Operation.cpp
+++ src/IR/Operation.cpp
@@ -13,6 +13,9 @@ BinaryOp::BinaryOp(IRContext &context, Value *lhs, Value *rhs) {
   assert(rhs && "BinaryOp rhs cannot be null");
   operands.emplace_back(lhs);
   operands.emplace_back(rhs);
+  // Add user for lhs and rhs.
+  lhs->addUser(this);
+  rhs->addUser(this);
 }
 
 AddOp::AddOp(IRContext &context, Value *lhs, Value *rhs)
@@ -97,6 +100,7 @@ BitOrOp::BitOrOp(IRContext &context, Value *lhs, Value *rhs)
 ReturnOp::ReturnOp(IRContext &context, Value *retVal) {
   if (retVal) {
     operands.emplace_back(retVal);
+    retVal->addUser(this);
     resultType = retVal->getType();
   } else {
     resultType = VoidType::get(context);
